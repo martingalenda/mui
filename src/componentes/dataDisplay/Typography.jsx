56bc0f4f80e6fee0cda8f2d7bfaff3dd4a0772bf
Martín G <martinarielgalenda@gmail.com>
2023-06-12 15:26:33 -0300

    seteando Design systemd de claro

--- src/componentes/dataDisplay/Typography.jsx
+++ src/componentes/dataDisplay/Typography.jsx
@@ -4,19 +4,38 @@ const TypographyMUI = () => {
     return (
         <Box sx={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
             <Typography> p (default) </Typography>
+            <br/>
             <Typography variant="h1"> H1 </Typography>
+            <br/>
             <Typography variant="h2"> H2 </Typography>
+            <br/>
             <Typography variant="h3"> H3 </Typography>
+            <br/>
             <Typography variant="h4"> H4 </Typography>
+            <br/>
             <Typography variant="h5"> H5 </Typography>
+            <br/>
             <Typography variant="h6"> H6 </Typography>
+            <br/>
             <Typography variant="subTitle1"> Sub Title 1 </Typography>
-            <Typography variant="subTitle2"> Sub-title 2 </Typography>
-            <Typography variant="body1"> Body 1 </Typography>
-            <Typography variant="body2"> Body 2 </Typography>
+            <br/>
+            <Typography variant="subTitle2"> Sub Title 2 </Typography>
+            <br/>
+            <Typography variant="body1"> Body 1 Regular </Typography>
+            <br/>
+            <Typography variant="body1"> Body 1 SemiBold </Typography>
+            <br/>
+            <Typography variant="body2"> Body 2 Regular</Typography>
+            <br/>
+            <Typography variant="body2"> Body 2 SemiBold</Typography>
+            <br/>
             <Typography variant="button"> Button </Typography>
+            <br/>
             <Typography variant="caption"> Caption </Typography>
+            <br/>
             <Typography variant="overline"> Overline </Typography>
+            <br/>
+            <br/>
             <Typography variant="h1" component="p"> Parrafo con apariencia de h1 </Typography>
         </Box>
     )
--- src/config/theme.config.jsx
+++ src/config/theme.config.jsx
@@ -1,45 +1,150 @@
-import { createTheme } from "@mui/material";
-import {  ThemeProvider} from "@mui/material";
-import {  CssBaseline} from "@mui/material";
+import {createTheme} from "@mui/material";
+import {ThemeProvider} from "@mui/material";
+import {CssBaseline} from "@mui/material";
 
 import PropTypes from 'prop-types';
 
+import {primary, secondary, states} from './palette.config' 
+
 const theme = createTheme({
-    // Colores:
+    // * Colores *
     palette: {
-      mode: 'dark',
+      mode: 'light',
       background: {
-        default: '#101010',
-        paper: '#050404',
+        default: primary.gray4,
+        paper: primary.gray5,
       },
       primary: {
-        main: '#c3a558',
-        dark: '#f9d781'
+        light: primary.lightBlue,
+        main: primary.instBlue,
+        dark: primary.darkBlue,
+        gray1: primary.gray1,
       },
       secondary: {
         main: '#c75154',
         dark: '#fe6d71'
       },
-      divider: '#494949'
+      text: {
+        primary: primary.gray1,
+        secondary: primary.gray2,
+        disabled: primary.gray3,
+      },
+      error: {
+        main: secondary.baseRed,
+        light: secondary.lightRed,
+        dark: secondary.darkRed,
+      },
+      warning: {
+        main: states.baseOrange,
+        light: states.lightOrange,
+      },
+      success: {
+        main: states.baseGreen,
+        light: states.lightGreen,
+      },
+      divider: primary.gray3,
     },
-    // Tipografia
+
+    // * Tipografia *
     typography:{
-        fontFamily: "'Blinker', sans-serif",
+        fontFamily: "'Open Sans', sans-serif",
+        htmlFontSize: 16,
+        fontWeightRegular: 400,
+        fontWeightMedium: 600,
+        fontWeightBold: 700,
+        color: primary.gray1,
+        // ! Títulos principales
         h1: {
-          fontSize: '6em',
+          fontSize: '1.5em',
+          color: primary.gray1,
+          letterSpacing: '0',
+          lineHeight: '1.2',
+          fontWeight: '600',
         },
+        // ! Títulos secundarios
         h2: {
-          fontSize: '5em',
-          color: 'blue',
+          fontSize: '1em',
+          color: primary.gray2,
+          letterSpacing: '0',
+          lineHeight: '1.2',
+          fontWeight: '600',
         },
         h3: {
-          fontSize: '4em',
+          fontSize: '1em',
+          color: primary.gray2,
+          letterSpacing: '0',
+          lineHeight: '1.2',
+          fontWeight: '600',
         },
         h4: {
-          fontSize: '3em',
+          fontSize: '1em',
+          color: primary.gray2,
+          letterSpacing: '0',
+          lineHeight: '1.2',
+          fontWeight: '600',
         },
+        h5: {
+          fontSize: '1em',
+          color: primary.gray2,
+          letterSpacing: '0',
+          lineHeight: '1.2',
+          fontWeight: '600',
+        },
+        h6: {
+          fontSize: '1em',
+          color: primary.gray2,
+          letterSpacing: '0',
+          lineHeight: '1.2',
+          fontWeight: '600',
+        },
+        subTitle1: {
+          fontSize: '1em',
+          color: primary.gray2,
+          letterSpacing: '0',
+          lineHeight: '1.2',
+          fontWeight: '600',
+        },
+        subTitle2: {
+          fontSize: '1em',
+          color: primary.gray2,
+          letterSpacing: '0',
+          lineHeight: '1.2',
+          fontWeight: '600',
+        },
+        // ! Cuerpo 1 Regular
+        // Texto general - Placeholders - Inputs - Campos de datos
+        body1: {
+          fontSize: '1em',
+          fontWeight: '400',
+          letterSpacing: '0',
+          lineHeight: '1.2',
+        },
+        // Texto resaltado - Texto datos relevantes
+        body1SB: {
+          fontSize: '0.75em',
+          fontWeight: '600',
+          letterSpacing: '0',
+          lineHeight: '1.2',
+        },
+        
+        // ! Cuerpo 1 Semibold
+        // Inputs labels - Captions - Títulos columnas
+        body2: {
+          fontSize: '0.75em',
+          fontWeight: '400',
+          letterSpacing: '0',
+          lineHeight: '1.2',
+        },
+        // Texto columnas - Texto resaltado en tablas - Tooltips
+        body2SB: {
+          fontSize: '0.75em',
+          fontWeight: '600',
+          letterSpacing: '0',
+          lineHeight: '1.2',
+        }
     },
-    // Componentes
+
+    // * Componentes *
     components: {
         MuiButton: {
             defaultProps: {
@@ -87,7 +192,8 @@ const theme = createTheme({
           ],
         },
     },
-    // Breakpoints
+
+    // * Breakpoints *
     breakpoints: {
       values: {
         xs: 0,
